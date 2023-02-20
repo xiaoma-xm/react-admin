968fc6f3991225e866e6a0388f0b9f21d755a7d5
xiaoma <2632571622@qq.com>
2023-02-20 14:25:45 +0800

    完成404页面

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,9 +1,17 @@
-
+import { Result, Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div>NotFound</div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button type="primary" onClick={() => navigate('/')}>Back Home</Button>}
+    />
   )
 }
 
-export default NotFound
+export default NotFound;
