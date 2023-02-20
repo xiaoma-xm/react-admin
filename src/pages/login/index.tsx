a1e18eeb8414b429c4c64b4b71b755576c36f47a
xiaoma <2632571622@qq.com>
2023-02-20 14:37:52 +0800

    完成登录静态页面

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,25 @@
-
+import { Form, Input, Button } from 'antd';
+import { LoginWrapper } from './styled';
 
 function Login() {
   return (
-    <div>Login</div>
+    <LoginWrapper>
+      <Form className='loginForm'>
+        <h1 className='loginTitle'>React Admin</h1>
+
+        <Form.Item>
+          <Input placeholder='请输入用户名' />
+        </Form.Item>
+
+        <Form.Item>
+          <Input.Password placeholder='请输入密码' />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type='primary' block>登录</Button>
+        </Form.Item>
+      </Form>
+    </LoginWrapper>
   )
 }
 
---
+++ src/pages/login/styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const LoginWrapper = styled.div`
+    overflow: hidden;
+    
+    .loginForm {
+        width: 600px;
+        margin: 200px auto;
+
+        .loginTitle {
+            text-align: center;
+            margin: 20px 0;
+        }
+    }
+`;
