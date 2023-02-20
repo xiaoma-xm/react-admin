031e40fd9f59a95cea174b1e1f33a288d8ce5601
xiaoma <2632571622@qq.com>
2023-02-20 14:46:25 +0800

    完成登录页表单数据收集和初始输入框聚焦

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,22 +1,39 @@
-import { Form, Input, Button } from 'antd';
+import { useEffect, useRef } from 'react';
+import { Form, Input, Button, type InputRef } from 'antd';
 import { LoginWrapper } from './styled';
 
+interface Ival {
+  username: string,
+  password: string
+}
+
 function Login() {
+  const inputRef = useRef<InputRef>(null);
+  
+  // 登录的回调处理
+  function submitHandler (val: Ival) {
+    console.log(val);
+  }
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+  
   return (
     <LoginWrapper>
-      <Form className='loginForm'>
+      <Form className='loginForm' onFinish={submitHandler}>
         <h1 className='loginTitle'>React Admin</h1>
 
-        <Form.Item>
-          <Input placeholder='请输入用户名' />
+        <Form.Item name='username'>
+          <Input ref={inputRef} placeholder='请输入用户名' />
         </Form.Item>
 
-        <Form.Item>
+        <Form.Item name='password'>
           <Input.Password placeholder='请输入密码' />
         </Form.Item>
 
         <Form.Item>
-          <Button type='primary' block>登录</Button>
+          <Button type='primary' block htmlType='submit'>登录</Button>
         </Form.Item>
       </Form>
     </LoginWrapper>
