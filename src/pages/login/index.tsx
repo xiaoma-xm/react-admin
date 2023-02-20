45e84d2098b68733dadceb4e83c642542a824e36
xiaoma <2632571622@qq.com>
2023-02-20 15:04:25 +0800

    完成登录页的表单校验

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,28 +7,41 @@ interface Ival {
   password: string
 }
 
+// 表单校验规则
+const rules = {
+  username: [
+    { required: true, message: '请输入用户名！' },
+    { pattern: /^[^\s]*$/, message: '请不要输入空格！' }
+  ],
+  password: [
+    { required: true, message: '请输入密码！' },
+    { pattern: /^[^\s]*$/, message: '请不要输入空格！' }
+  ]
+};
+
+// 组件
 function Login() {
   const inputRef = useRef<InputRef>(null);
-  
+
   // 登录的回调处理
-  function submitHandler (val: Ival) {
+  function submitHandler(val: Ival) {
     console.log(val);
   }
 
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
-  
+
   return (
     <LoginWrapper>
       <Form className='loginForm' onFinish={submitHandler}>
         <h1 className='loginTitle'>React Admin</h1>
 
-        <Form.Item name='username'>
+        <Form.Item name='username' rules={rules.username}>
           <Input ref={inputRef} placeholder='请输入用户名' />
         </Form.Item>
 
-        <Form.Item name='password'>
+        <Form.Item name='password' rules={rules.password}>
           <Input.Password placeholder='请输入密码' />
         </Form.Item>
 
