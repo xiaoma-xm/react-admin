a9db38adfe991132f3cf0d607956df0eb7f2bc3d
xiaoma <2632571622@qq.com>
2023-02-22 15:17:35 +0800

    home页可视化（未完成）

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,70 @@
-
+import { useRef, useEffect } from "react";
+import { Col, Row } from "antd";
+import { HomeWrapper } from "./styled";
+import * as echarts from 'echarts';
 
 function Home() {
+  const barChartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // echarts初始化容器
+    const chart = echarts.init(barChartRef.current as HTMLDivElement);
+    // 图表配置项
+    let options = {
+      title: {
+        text: "衣服和鞋子",
+      },
+      tooltip: {},
+      legend: {
+        data: ["销量"],
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    };
+    chart.setOption(options);
+  }, []);
+
   return (
-    <div>Home</div>
+    <HomeWrapper>
+      <Row className="mb-10">
+        <Col className="col mr-10" span={6}>
+          <div className="barChart" ref={barChartRef}>图表1</div>
+        </Col>
+        <Col className="col mr-10" span={6}>
+          <div>图表2</div>
+        </Col>
+        <Col className="col mr-10" span={6}>
+          <div>图表3</div>
+        </Col>
+        <Col className="col" span={5}>
+          <div>图表4</div>
+        </Col>
+      </Row>
+
+      <Row className="mb-10">
+        <Col className="col mr-10" span={14}>
+          <div>图表5</div>
+        </Col>
+        <Col className="col" span={9}>
+          <div>图表6</div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="col" span={24}>
+          <div>图表7</div>
+        </Col>
+      </Row>
+    </HomeWrapper>
   )
 }
 
---
+++ src/pages/home/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HomeWrapper = styled.div`
+    .col {
+        padding: 10px;
+        box-shadow: 2px 2px 10px rgba(0, 0, 0, .1);
+
+        .barChart {
+            height: 200px;
+        }
+    }
+`;
